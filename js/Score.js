614dc3bb33f690e228a6d5f31c5aaf3919e43a0a
Risi Ferraz <risiferraz@gmail.com>
2022-01-29 14:37:50 -0300

    Salvando a Lista de Pontuação

--- js/Score.js
+++ js/Score.js
@@ -1,14 +1,22 @@
 class Score{
     constructor (){
-        document.getElementById('envia-pontuacao').addEventListener('click',()=>this.pegaPontuacao())
+        document.getElementById('envia-pontuacao').addEventListener('click',()=>this.enviaPontuacao())
     }
-    pegaPontuacao(){
+    enviaPontuacao(){
+        const listaDePontuacao = this._pegaListaDePontuacao()
         const nome = document.getElementById ("nome-jogador").value
         const pontuacao = document.getElementById ("pontuacao-final").value
         const jogadorRank= {nome,pontuacao}
-        window.localStorage.setItem('listaDeScore', JSON.stringify(jogadorRank))
-       
+        listaDePontuacao.push(jogadorRank)
+        window.localStorage.setItem('listaDeScore', JSON.stringify(listaDePontuacao))
     }
+    _pegaListaDePontuacao(){
+        const listaDeScoreJSON = window.localStorage.getItem('listaDeScore')
+        if (!!listaDeScoreJSON){
+           return JSON.parse(listaDeScoreJSON)
+        }else{
+            return[]
+        }
 
-
+    }
 }
